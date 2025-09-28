c0670a794b293d86d1cd5f1d733823eb53ba3faa
Dera_Jason <derajasochuks@gmail.com>
2025-09-28 19:20:24 +0100

    Add Django project with relationship_app and user authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    register_view,
-    login_view,
-    logout_view
-)
+from .views import register_view, list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),                # FBV
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # CBV
+
+    # Authentication URLs
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
